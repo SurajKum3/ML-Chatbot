c0d1c1661983a0533b6e1d2a89cddd1c491add0a
suraj <surajsinghjapla@gmail.com>
2026-02-14 19:37:51 +0530

    Requirements Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ml_chatbot',
+    version='0.1.0',
+    author='Suraj Singh',
+    author_email='surajsinghjapla@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
